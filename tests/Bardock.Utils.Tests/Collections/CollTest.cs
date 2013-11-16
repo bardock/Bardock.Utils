9f8e1ae2d2286f6e0efc2c07b2b50d44ed4fcf5b
bardock <fedesanchez89@gmail.com>
2013-11-16 13:46:23 -0800

    Collections tests

---
+++ tests/Bardock.Utils.Tests/Collections/CollTest.cs
@@ -0,0 +1,26 @@
+﻿using Bardock.Utils.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Bardock.Utils.Tests.Collections
+{
+    public class CollTest
+    {
+        [Fact]
+        public void Array()
+        {
+            var r = Coll.Array(1, 2, 3, 3, 5);
+
+            Assert.Equal(5, r.Length);
+            Assert.Equal(1, r[0]);
+            Assert.Equal(2, r[1]);
+            Assert.Equal(3, r[2]);
+            Assert.Equal(3, r[3]);
+            Assert.Equal(5, r[4]);
+        }
+    }
+}
---
+++ tests/Bardock.Utils.Tests/Collections/DisposableListTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using System.Linq;
+
+namespace Bardock.Utils.Collections
+{
+	public class DisposableListTest
+	{
+        private class MyDisposable : IDisposable
+        {
+            public bool IsDisposed { get; private set; }
+
+            public void Dispose()
+            {
+                IsDisposed = true;
+            }
+        }
+
+        [Fact]
+        public void Dispose()
+        {
+            var list = new DisposableList<MyDisposable>();
+
+            Assert.True(list.All(x => !x.IsDisposed));
+
+            list.Dispose();
+
+            Assert.True(list.All(x => x.IsDisposed));
+        }
+	}
+}
