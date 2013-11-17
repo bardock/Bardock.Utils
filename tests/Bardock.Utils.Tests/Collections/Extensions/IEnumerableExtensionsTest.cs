c739f596a3c3367377883d62e85983f5acfc8fc3
bardock <fedesanchez89@gmail.com>
2013-11-17 05:27:46 -0800

    IEnumerableExtension tests - split without clearEmpty

--- tests/Bardock.Utils.Tests/Collections/Extensions/IEnumerableExtensionsTest.cs
+++ tests/Bardock.Utils.Tests/Collections/Extensions/IEnumerableExtensionsTest.cs
@@ -22,7 +22,7 @@ namespace Bardock.Utils.Tests.Collections.Extensions
 		}
 
         [Fact]
-        public void Split_EmptyCleared()
+        public void Split_Empty_ClearEmpty()
         {
             var empty = Coll.Array<int>();
             var r = empty.Split(x => x == 1, clearEmpty: true);
@@ -33,6 +33,31 @@ namespace Bardock.Utils.Tests.Collections.Extensions
 
         [Fact]
         public void Split()
+        {
+            var list = Coll.Array(1, 2, 2, 3, 4, 5, 6, 7, 8);
+            var r = list.Split(x => x == 2 || x == 4 || x == 8);
+
+            Assert.NotNull(r);
+            Assert.Equal(5, r.Count());
+
+            Assert.Equal(1, r.ElementAt(0).Count());
+            Assert.Equal(1, r.ElementAt(0).ElementAt(0));
+
+            Assert.Equal(0, r.ElementAt(1).Count());
+
+            Assert.Equal(1, r.ElementAt(2).Count());
+            Assert.Equal(3, r.ElementAt(2).ElementAt(0));
+
+            Assert.Equal(3, r.ElementAt(3).Count());
+            Assert.Equal(5, r.ElementAt(3).ElementAt(0));
+            Assert.Equal(6, r.ElementAt(3).ElementAt(1));
+            Assert.Equal(7, r.ElementAt(3).ElementAt(2));
+
+            Assert.Equal(0, r.ElementAt(4).Count());
+        }
+
+        [Fact]
+        public void Split_ClearEmpty()
         {
             var list = Coll.Array(1, 2, 2, 3, 4, 5, 6, 7, 8);
             var r = list.Split(x => x == 2 || x == 4 || x == 8, clearEmpty: true);
