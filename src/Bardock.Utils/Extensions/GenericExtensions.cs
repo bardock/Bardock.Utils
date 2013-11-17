bc9bd82ff94cb2f7606331fe68ac0deafbf95d47
bardock <fedesanchez89@gmail.com>
2013-11-17 08:41:58 -0800

    renames ObjectExtensions to GenericExtensions

---
+++ src/Bardock.Utils/Extensions/GenericExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Bardock.Utils.Extensions
+{
+	public static class GenericExtensions
+	{
+        /// <summary>
+        /// Determines if the instance is equal to any of the specified values
+        /// </summary>
+        public static bool In<T>(this T @this, IEnumerable<T> evaluate)
+        {
+            return @this != null && evaluate.Any(x => @this.Equals(x));
+        }
+
+        /// <summary>
+        /// Determines if the instance is equal to any of the specified values
+        /// </summary>
+        public static bool In<T>(this T @this, params T[] evaluate)
+        {
+            return @this.In(evaluate as IEnumerable<T>);
+        }
+	}
+}
