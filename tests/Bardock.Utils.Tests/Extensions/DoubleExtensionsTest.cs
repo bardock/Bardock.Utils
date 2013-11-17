5f96f4ee3b8f43b7d68bcc00a610a1a916d57c6a
bardock <fedesanchez89@gmail.com>
2013-11-17 08:02:41 -0800

    DoubleExtensions test

---
+++ tests/Bardock.Utils.Tests/Extensions/DoubleExtensionsTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Bardock.Utils.Extensions;
+using Bardock.Utils.Globalization;
+
+namespace Bardock.Utils.Tests.Extensions
+{
+    public class DoubleExtensionsTest
+    {
+        private const decimal DEC1 = 123456.789M;
+
+        [Fact]
+        public void ToInvariantString()
+        {
+            var r = DEC1.ToInvariantString();
+            Assert.Equal("123456.789", r);
+        }
+    }
+}
