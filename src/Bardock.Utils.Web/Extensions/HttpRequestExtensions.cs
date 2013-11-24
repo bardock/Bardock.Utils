649d084ff8d3c9dfa367c6b83269e84fb41dac9c
bardock <fedesanchez89@gmail.com>
2013-11-24 14:45:31 -0800

    HttpRequest extensions for checking each HttpMethod

--- src/Bardock.Utils.Web/Extensions/HttpRequestExtensions.cs
+++ src/Bardock.Utils.Web/Extensions/HttpRequestExtensions.cs
@@ -11,42 +11,42 @@ namespace Bardock.Utils.Web.Extensions
                 || ((request.Headers != null) && (request.Headers["X-Requested-With"] == "XMLHttpRequest"));
         }
 
-        public bool IsPost(this HttpRequest req)
+        public static bool IsPost(this HttpRequest req)
         {
             return string.Equals(req.HttpMethod, "POST", StringComparison.InvariantCultureIgnoreCase);
         }
 
-        public bool IsGet(this HttpRequest req)
+        public static bool IsGet(this HttpRequest req)
         {
             return string.Equals(req.HttpMethod, "GET", StringComparison.InvariantCultureIgnoreCase);
         }
 
-        public bool IsDelete(this HttpRequest req)
+        public static bool IsDelete(this HttpRequest req)
         {
             return string.Equals(req.HttpMethod, "DELETE", StringComparison.InvariantCultureIgnoreCase);
         }
 
-        public bool IsPut(this HttpRequest req)
+        public static bool IsPut(this HttpRequest req)
         {
             return string.Equals(req.HttpMethod, "PUT", StringComparison.InvariantCultureIgnoreCase);
         }
 
-        public bool IsPost(this HttpRequestBase req)
+        public static bool IsPost(this HttpRequestBase req)
         {
             return string.Equals(req.HttpMethod, "POST", StringComparison.InvariantCultureIgnoreCase);
         }
 
-        public bool IsGet(this HttpRequestBase req)
+        public static bool IsGet(this HttpRequestBase req)
         {
             return string.Equals(req.HttpMethod, "GET", StringComparison.InvariantCultureIgnoreCase);
         }
 
-        public bool IsDelete(this HttpRequestBase req)
+        public static bool IsDelete(this HttpRequestBase req)
         {
             return string.Equals(req.HttpMethod, "DELETE", StringComparison.InvariantCultureIgnoreCase);
         }
 
-        public bool IsPut(this HttpRequestBase req)
+        public static bool IsPut(this HttpRequestBase req)
         {
             return string.Equals(req.HttpMethod, "PUT", StringComparison.InvariantCultureIgnoreCase);
         }
