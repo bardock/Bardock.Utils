1c5caf33f1de80951912febe11becb989c2efd1b
bardock <fedesanchez89@gmail.com>
2013-11-24 14:48:01 -0800

    WebRequestExtensions.ReadAllResponseString

---
+++ src/Bardock.Utils/Extensions/WebRequestExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Bardock.Utils.Extensions
+{
+    public static class WebRequestExtensions
+    {
+        public static string ReadAllResponseString(this WebRequest request)
+        {
+            using(var response = request.GetResponse())
+            {
+                using(var responseStream = response.GetResponseStream())
+                {
+                    using(var reader = new StreamReader(responseStream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
