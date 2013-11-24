cfb9936c7528ff4ce7b4796ac79e3b3a14994690
bardock <fedesanchez89@gmail.com>
2013-11-24 14:50:23 -0800

    IntegerExtensions for formatting with thousand separators

---
+++ src/Bardock.Utils/Extensions/IntegerExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Bardock.Utils.Extensions
+{
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Returns string representation with thousand separators
+        /// </summary>
+        public static string Format(this int n)
+        {
+            return n.ToString("#,0");
+        }
+
+        /// <summary>
+        /// Returns string representation with thousand separators
+        /// </summary>
+        public static string Format(this long n)
+        {
+            return n.ToString("#,0");
+        }
+    }
+}
