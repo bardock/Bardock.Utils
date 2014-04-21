4c427d70cf93b15d75ab2850e2c12b45a88f334d
fsanchez-originsoftware <fsanchez@originsoftware.com>
2014-04-21 12:49:34 -0300

    nuget pub

---
+++ src/Bardock.Utils/Bardock.Utils.pub.bat
@@ -0,0 +1,2 @@
+nuget pack -sym Bardock.Utils.csproj
+nuget push Bardock.Utils.1.0.0.0.nupkg
--- src/Bardock.Utils/Properties/AssemblyInfo.cs
+++ src/Bardock.Utils/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bardock.Utils")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A set of utilities")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("bardock")]
 [assembly: AssemblyProduct("Bardock.Utils")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
