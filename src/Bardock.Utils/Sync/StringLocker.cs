396355ea2e83232c04220dedfc83bd7dc3554009
fsanchez-originsoftware <fsanchez@originsoftware.com>
2014-06-18 14:55:01 -0300

    Lockers replaced by StringLocker 1.2.0.0

---
+++ src/Bardock.Utils/Sync/StringLocker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Bardock.Utils.Sync
+{
+    /// <summary>
+    /// Provides a way to lock based on a string. 
+    /// Instanciate one StringLocker for every use case you might have.
+    /// </summary>
+    public class StringLocker
+    {
+        // More info: http://stackoverflow.com/a/19375402
+
+        private readonly ConcurrentDictionary<string, string> _locks =
+            new ConcurrentDictionary<string, string>();
+
+        public string GetLockObject(string s)
+        {
+            return _locks.GetOrAdd(s, String.Copy);
+        }
+    }
+}
