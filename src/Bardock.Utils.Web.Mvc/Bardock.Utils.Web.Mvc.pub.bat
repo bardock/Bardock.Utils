2bccf198121764d9c8df4e88588529741c0234a8
fsanchez-originsoftware <fsanchez@originsoftware.com>
2014-06-27 16:23:00 -0300

    Bardock.Utils.Web.Mvc version 1.2.0

--- src/Bardock.Utils.Web.Mvc/Bardock.Utils.Web.Mvc.pub.bat
+++ src/Bardock.Utils.Web.Mvc/Bardock.Utils.Web.Mvc.pub.bat
@@ -1,5 +1,5 @@
 :: nuget spec
 set /p version=Version number:
-nuget pack Bardock.Utils.Web.Mvc.csproj -Prop Configuration=Release -IncludeReferencedProjects -Version %version%
+nuget pack Bardock.Utils.Web.Mvc.csproj -Prop Configuration=Debug -Symbols -IncludeReferencedProjects -Version %version%
 nuget push Bardock.Utils.Web.Mvc.%version%.nupkg
 pause;
