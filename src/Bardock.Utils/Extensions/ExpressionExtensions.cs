328f203f49a3b45792cb97b97316971aa5ec311d
fsanchez-originsoftware <fsanchez@originsoftware.com>
2014-07-28 12:01:58 -0300

    ExpressionExtensions - PartialApply last param

--- src/Bardock.Utils/Extensions/ExpressionExtensions.cs
+++ src/Bardock.Utils/Extensions/ExpressionExtensions.cs
@@ -10,24 +10,31 @@ namespace Bardock.Utils.Extensions
         {
             private readonly LambdaExpression _expr;
             private readonly A1 _arg1;
-            public PartialApplyVisitor(LambdaExpression expr, A1 arg1)
+            private ParameterExpression _paramToApply;
+
+            public PartialApplyVisitor(LambdaExpression expr, A1 arg1, ParameterExpression paramToApply = null)
             {
-                _expr = expr;
-                _arg1 = arg1;
+                this._expr = expr;
+                this._arg1 = arg1;
+                this._paramToApply = paramToApply ?? expr.Parameters.First();
             }
+
             public Expression Visit()
             {
                 return this.Visit(_expr.Body);
             }
+
             protected override Expression VisitParameter(ParameterExpression node)
             {
-                if (node.Equals(_expr.Parameters.First()))
+                if (node.Equals(this._paramToApply))
                 {
                     return Expression.Constant(_arg1);
                 }
                 return base.VisitParameter(node);
             }
         }
+
+        #region PartialApply_Func
 
         public static Expression<Func<R>> PartialApply<A1, R>(
             this Expression<Func<A1, R>> expr, A1 arg1)
@@ -40,15 +47,41 @@ namespace Bardock.Utils.Extensions
             this Expression<Func<A1, A2, R>> expr, A1 arg1)
         {
             return Expression.Lambda<Func<A2, R>>(
-                new PartialApplyVisitor<A1>(expr, arg1).Visit(), expr.Parameters.Skip(1));
+                new PartialApplyVisitor<A1>(expr, arg1).Visit(), 
+                expr.Parameters.Skip(1));
         }
 
         public static Expression<Func<A2, A3, R>> PartialApply<A1, A2, A3, R>(
             this Expression<Func<A1, A2, A3, R>> expr, A1 arg1)
         {
             return Expression.Lambda<Func<A2, A3, R>>(
-                new PartialApplyVisitor<A1>(expr, arg1).Visit(), expr.Parameters.Skip(2));
+                new PartialApplyVisitor<A1>(expr, arg1).Visit(), 
+                expr.Parameters.Skip(1));
         }
+
+        #region PartialApply_Func_LastParam
+
+        public static Expression<Func<A1, R>> PartialApply<A1, A2, R>(
+            this Expression<Func<A1, A2, R>> expr, A2 arg2)
+        {
+            return Expression.Lambda<Func<A1, R>>(
+                new PartialApplyVisitor<A2>(expr, arg2, expr.Parameters.Last()).Visit(),
+                expr.Parameters.Take(1));
+        }
+
+        public static Expression<Func<A1, A2, R>> PartialApply<A1, A2, A3, R>(
+            this Expression<Func<A1, A2, A3, R>> expr, A3 arg3)
+        {
+            return Expression.Lambda<Func<A1, A2, R>>(
+                new PartialApplyVisitor<A3>(expr, arg3, expr.Parameters.Last()).Visit(),
+                expr.Parameters.Take(2));
+        }
+
+        #endregion
+
+        #endregion
+
+        #region PartialApply_Action
 
         public static Expression<Action> PartialApply<A1>(
             this Expression<Action<A1>> expr, A1 arg1)
@@ -61,14 +94,38 @@ namespace Bardock.Utils.Extensions
             this Expression<Action<A1, A2>> expr, A1 arg1)
         {
             return Expression.Lambda<Action<A2>>(
-                new PartialApplyVisitor<A1>(expr, arg1).Visit(), expr.Parameters.Skip(1));
+                new PartialApplyVisitor<A1>(expr, arg1).Visit(), 
+                expr.Parameters.Skip(1));
         }
 
         public static Expression<Action<A2, A3>> PartialApply<A1, A2, A3>(
             this Expression<Action<A1, A2, A3>> expr, A1 arg1)
         {
             return Expression.Lambda<Action<A2, A3>>(
-                new PartialApplyVisitor<A1>(expr, arg1).Visit(), expr.Parameters.Skip(2));
+                new PartialApplyVisitor<A1>(expr, arg1).Visit(), 
+                expr.Parameters.Skip(1));
         }
+
+        #region PartialApply_Action_LastParam
+
+        public static Expression<Action<A1>> PartialApplyLast<A1, A2>(
+            this Expression<Action<A1, A2>> expr, A2 arg2)
+        {
+            return Expression.Lambda<Action<A1>>(
+                new PartialApplyVisitor<A2>(expr, arg2, expr.Parameters.Last()).Visit(), 
+                expr.Parameters.Take(1));
+        }
+
+        public static Expression<Action<A1, A2>> PartialApplyLast<A1, A2, A3>(
+            this Expression<Action<A1, A2, A3>> expr, A3 arg3)
+        {
+            return Expression.Lambda<Action<A1, A2>>(
+                new PartialApplyVisitor<A3>(expr, arg3, expr.Parameters.Last()).Visit(), 
+                expr.Parameters.Take(2));
+        }
+
+        #endregion
+
+        #endregion
     }
 }
---
+++ tests/Bardock.Utils.Tests/Extensions/ExpressionExtensionsTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq.Expressions;
+using Bardock.Utils.Extensions;
+using Xunit;
+
+namespace Bardock.Utils.Tests.Extensions
+{
+    public class ExpressionExtensionsTest
+    {
+        [Fact]
+        public void PartialApply_Func_1Args()
+        {
+            Expression<Func<string, string>> exp = (x => x + "_suffix");
+            var r = exp.PartialApply("test");
+            Assert.Equal("() => (\"test\" + \"_suffix\")", r.ToString());
+        }
+
+        [Fact]
+        public void PartialApply_Func_2Args()
+        {
+            Expression<Func<string, int, string>> exp = ((x, y) => x + y.ToString() + "_suffix");
+            var r = exp.PartialApply("test");
+            Assert.Equal("y => ((\"test\" + y.ToString()) + \"_suffix\")", r.ToString());
+        }
+
+        [Fact]
+        public void PartialApply_Func_3Args()
+        {
+            Expression<Func<string, int, DateTime, string>> exp = ((x, y, z) => x + y.ToString() + z.ToString() + "_suffix");
+            var r = exp.PartialApply("test");
+            Assert.Equal("(y, z) => (((\"test\" + y.ToString()) + z.ToString()) + \"_suffix\")", r.ToString());
+        }
+
+        [Fact]
+        public void PartialApply_Func_2Args_LastParam()
+        {
+            Expression<Func<string, int, string>> exp = ((x, y) => x + y.ToString() + "_suffix");
+            var r = exp.PartialApply(1);
+            Assert.Equal("x => ((x + 1.ToString()) + \"_suffix\")", r.ToString());
+        }
+
+        [Fact]
+        public void PartialApply_Func_3Args_LastParam()
+        {
+            Expression<Func<string, int, DateTime, string>> exp = ((x, y, z) => x + y.ToString() + z.ToString() + "_suffix");
+            var date = new DateTime(2000,1,1);
+            var r = exp.PartialApply(date);
+            Assert.Equal("(x, y) => (((x + y.ToString()) + "+date+".ToString()) + \"_suffix\")", r.ToString());
+        }
+    }
+}
