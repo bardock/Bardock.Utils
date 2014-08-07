2603627a94145c693da4b08b3d0780bcfbb57919
fsanchez-originsoftware <fsanchez@originsoftware.com>
2014-08-07 19:27:48 -0300

    ConstantExpressionExtensions

---
+++ src/Bardock.Utils/Extensions/Expressions/ConstantExpressionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Bardock.Utils.Extensions
+{
+    public static class ConstantExpressionExtensions
+    {
+        /// <summary>
+        /// Determines if expression is a constant with specified value
+        /// </summary>
+        public static bool IsConstant<R>(
+            this Expression expr,
+            R value)
+        {
+            var constExpr = expr as ConstantExpression;
+            return constExpr != null && constExpr.Value.Equals(value);
+        }
+
+        /// <summary>
+        /// Determines if expression is a constant with specified value
+        /// </summary>
+        public static bool IsConstant<R>(
+            this Expression<Func<R>> expr,
+            R value)
+        {
+            return expr.Body.IsConstant(value);
+        }
+
+        /// <summary>
+        /// Determines if expression is a constant with specified value
+        /// </summary>
+        public static bool IsConstant<A1, R>(
+            this Expression<Func<A1, R>> expr,
+            R value)
+        {
+            return expr.Body.IsConstant(value);
+        }
+    }
+}
---
+++ tests/Bardock.Utils.Tests/Extensions/Expressions/ConstantExpressionExtensionsTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq.Expressions;
+using Bardock.Utils.Extensions;
+using Xunit;
+
+namespace Bardock.Utils.Tests.Extensions
+{
+    public class ConstantExpressionExtensionsTest
+    {
+        [Fact]
+        public void IsConstant_Bool()
+        {
+            Expression<Func<string, bool>> exp = (x => true);
+            Assert.True(exp.IsConstant(true));
+        }
+
+        [Fact]
+        public void IsConstant_String()
+        {
+            Expression<Func<string, string>> exp = (x => "asd");
+            Assert.True(exp.IsConstant("asd"));
+        }
+
+        [Fact]
+        public void IsConstant_DifferentValue()
+        {
+            Expression<Func<bool, string>> exp = (x => "true");
+            Assert.False(exp.IsConstant("true "));
+        }
+
+        [Fact]
+        public void IsConstant_DifferentType()
+        {
+            Expression<Func<bool, string>> exp = (x => "true");
+            Assert.False(exp.IsConstant(true));
+        }
+
+        [Fact]
+        public void IsConstant_Method()
+        {
+            Expression<Func<bool, string>> exp = (x => x.ToString());
+            Assert.False(exp.IsConstant(""));
+        }
+    }
+}
