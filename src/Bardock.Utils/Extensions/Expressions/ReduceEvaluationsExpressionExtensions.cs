0b5356ac116d23660270c3e711c5524a5149c0f5
fsanchez-originsoftware <fsanchez@originsoftware.com>
2014-08-08 09:28:38 -0300

    ReduceEvaluationsExpressionVisitor

---
+++ src/Bardock.Utils/Extensions/Expressions/ReduceEvaluationsExpressionExtensions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Bardock.Utils.Extensions
+{
+    /// <summary>
+    /// Reduces constant logical evaluations.
+    /// Example:
+    ///     input:  x => x == "a" || true
+    ///     output: x => true
+    /// </summary>
+    public class ReduceEvaluationsExpressionVisitor : ExpressionVisitor
+    {
+        protected bool anyReduced;
+
+        /// <summary>
+        /// Recusively visits expression nodes until no one could be replaced
+        /// </summary>
+        public Expression ReduceAll(Expression expr)
+        {
+            do
+            {
+                anyReduced = false;
+                expr = this.Visit(expr);
+            } 
+            while (anyReduced);
+
+            return expr;
+        }
+
+        /// <summary>
+        /// Checks if expression node can be replaced by a constant (true or false).
+        /// </summary>
+        public override Expression Visit(Expression node)
+        {
+            var binExpr = node as BinaryExpression;
+            if (binExpr != null)
+            {
+                if (binExpr.NodeType == ExpressionType.OrElse
+                    && (binExpr.Left.IsConstant(true) || binExpr.Right.IsConstant(true)))
+                {
+                    anyReduced = true;
+                    return Expression.Constant(true);
+                }
+                if (binExpr.NodeType == ExpressionType.AndAlso
+                    && (binExpr.Left.IsConstant(false) || binExpr.Right.IsConstant(false)))
+                {
+                    anyReduced = true;
+                    return Expression.Constant(false);
+                }
+            }
+            return base.Visit(node);
+        }
+    }
+
+    public static class ReduceEvaluationsExpressionExtensions
+    {
+        /// <summary>
+        /// Reduces constant logical evaluations.
+        /// Example:
+        ///     input:  x => x == "a" || true
+        ///     output: x => true
+        /// </summary>
+        public static Expression<Func<A1, bool>> ReduceEvaluations<A1>(
+            this Expression<Func<A1, bool>> expr)
+        {
+            return Expression.Lambda<Func<A1, bool>>(
+                new ReduceEvaluationsExpressionVisitor().ReduceAll(expr.Body),
+                expr.Parameters);
+        }
+
+        /// <summary>
+        /// Reduces constant logical evaluations.
+        /// Example:
+        ///     input:  x => x == "a" || true
+        ///     output: x => true
+        /// </summary>
+        public static Expression<Func<A1, A2, bool>> ReduceEvaluations<A1, A2>(
+            this Expression<Func<A1, A2, bool>> expr)
+        {
+            return Expression.Lambda<Func<A1, A2, bool>>(
+                new ReduceEvaluationsExpressionVisitor().ReduceAll(expr.Body),
+                expr.Parameters);
+        }
+
+        /// <summary>
+        /// Reduces constant logical evaluations.
+        /// Example:
+        ///     input:  x => x == "a" || true
+        ///     output: x => true
+        /// </summary>
+        public static Expression<Func<A1, A2, A3, bool>> ReduceEvaluations<A1, A2, A3>(
+            this Expression<Func<A1, A2, A3, bool>> expr)
+        {
+            return Expression.Lambda<Func<A1, A2, A3, bool>>(
+                new ReduceEvaluationsExpressionVisitor().ReduceAll(expr.Body),
+                expr.Parameters);
+        }
+    }
+}
---
+++ tests/Bardock.Utils.Tests/Extensions/Expressions/ReduceEvaluationsExpressionExtensionsTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq.Expressions;
+using Bardock.Utils.Extensions;
+using Xunit;
+
+namespace Bardock.Utils.Tests.Extensions
+{
+    public class ReduceEvaluationsExpressionExtensionsTest
+    {
+        [Fact]
+        public void ReduceEvaluations_OrElse_1level()
+        {
+            Expression<Func<string, bool>> exp = (x => (false && x == "a") || true);
+            var r = exp.ReduceEvaluations();
+            Assert.Equal("x => True", r.ToString());
+        }
+
+        [Fact]
+        public void ReduceEvaluations_OrElse_2levels()
+        {
+            Expression<Func<string, bool>> exp = (x => (false && x == "a") || (x == "b" || true));
+            var r = exp.ReduceEvaluations();
+            Assert.Equal("x => True", r.ToString());
+        }
+
+        [Fact]
+        public void ReduceEvaluations_OrElse_3levels()
+        {
+            Expression<Func<string, bool>> exp = (x => (false && x == "a") || (x == "b" || (true || x == "c")));
+            var r = exp.ReduceEvaluations();
+            Assert.Equal("x => True", r.ToString());
+        }
+
+        [Fact]
+        public void ReduceEvaluations_AndAlso_1level()
+        {
+            Expression<Func<string, bool>> exp = (x => (true || x == "a") && false);
+            var r = exp.ReduceEvaluations();
+            Assert.Equal("x => False", r.ToString());
+        }
+
+        [Fact]
+        public void ReduceEvaluations_AndAlso_2levels()
+        {
+            Expression<Func<string, bool>> exp = (x => (true || x == "a") && (x == "b" && false));
+            var r = exp.ReduceEvaluations();
+            Assert.Equal("x => False", r.ToString());
+        }
+
+        [Fact]
+        public void ReduceEvaluations_AndAlso_3levels()
+        {
+            Expression<Func<string, bool>> exp = (x => (true || x == "a") && (x == "b" && (false && x == "c")));
+            var r = exp.ReduceEvaluations();
+            Assert.Equal("x => False", r.ToString());
+        }
+
+        [Fact]
+        public void ReduceEvaluations_Variable_3levels()
+        {
+            Expression<Func<string, bool>> exp = (x => (true || x == "a") && (x == "b" && (x == "c" || x == "d")));
+            var r = exp.ReduceEvaluations();
+            Assert.Equal("x => (True AndAlso ((x == \"b\") AndAlso ((x == \"c\") OrElse (x == \"d\"))))", r.ToString());
+        }
+    }
+}
