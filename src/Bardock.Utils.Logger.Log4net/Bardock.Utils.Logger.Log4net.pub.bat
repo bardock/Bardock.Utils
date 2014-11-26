0acab4ee8a28934d475ccdb10e69d952d57e449d
fsanchez-originsoftware <fsanchez@originsoftware.com>
2014-11-26 15:46:57 -0300

    fix nuget publish

--- src/Bardock.Utils.Logger.Log4net/Bardock.Utils.Logger.Log4net.pub.bat
+++ src/Bardock.Utils.Logger.Log4net/Bardock.Utils.Logger.Log4net.pub.bat
@@ -1,5 +1,5 @@
 :: nuget spec
 set /p version=Version number:
-nuget pack Bardock.Utils.Logger.Log4net.csproj -Prop Configuration=Release -IncludeReferencedProjects -Version %version%
+nuget pack Bardock.Utils.Logger.Log4net.csproj -Prop Configuration=Debug -Symbols -IncludeReferencedProjects -Version %version%
 nuget push Bardock.Utils.Logger.Log4net.%version%.nupkg
 pause;
--- src/Bardock.Utils.Logger/Bardock.Utils.Logger.pub.bat
+++ src/Bardock.Utils.Logger/Bardock.Utils.Logger.pub.bat
@@ -1,5 +1,5 @@
 :: nuget spec
 set /p version=Version number:
-nuget pack Bardock.Utils.Logger.csproj -Prop Configuration=Release -IncludeReferencedProjects -Version %version%
+nuget pack Bardock.Utils.Logger.csproj -Prop Configuration=Debug -Symbols -IncludeReferencedProjects -Version %version%
 nuget push Bardock.Utils.Logger.%version%.nupkg
 pause;
