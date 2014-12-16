70416d9529ed9953a212d21fb4330aa9df2317e4
fsanchez-originsoftware <fsanchez@originsoftware.com>
2014-12-16 12:49:45 -0300

    Add feature that combines two or more expressions that call a constructor and initialize one or more members (MemberInitExpressions)

---
+++ src/Bardock.Utils/Extensions/Expressions/MemberInitExpressionExtensions.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Bardock.Utils.Extensions.Expressions
+{
+    public static class MemberInitExpressionExtensions
+    {
+        /// <summary>
+        /// Combines two or more expressions that call a constructor and initialize one or more members (MemberInitExpressions)
+        /// </summary>
+        /// <typeparam name="TSource">Expressions parameter type</typeparam>
+        /// <typeparam name="TDestination1">Return type of first expression</typeparam>
+        /// <typeparam name="TDestination2">
+        /// Return type of second and next expressions.
+        /// TDestination2 must implement TDestination1 and it will be the combined expression return type</typeparam>
+        /// <param name="memberInitExpression">First expression</param>
+        /// <param name="memberInitExpressions">Other expressions to combine</param>
+        /// <returns>A MemberInitExpression with members from all specified expressions</returns>
+        public static Expression<Func<TSource, TDestination2>> AddMembers<TSource, TDestination1, TDestination2>(
+            this Expression<Func<TSource, TDestination1>> memberInitExpression,
+            params Expression<Func<TSource, TDestination2>>[] memberInitExpressions)
+            where TDestination2 : TDestination1
+        {
+            var param = Expression.Parameter(typeof(TSource), "x");
+
+            var emptyConstructor = typeof(TDestination2).GetConstructor(Type.EmptyTypes);
+            if (emptyConstructor == null)
+                throw new ArgumentException("TDestination2 must have an empty constructor");
+
+            return Expression.Lambda<Func<TSource, TDestination2>>(
+                body: Expression.MemberInit(
+                        newExpression: Expression.New(emptyConstructor),
+                        bindings: new[] { memberInitExpression }
+                            .ToMemberAssignments(param)
+                            .Concat(memberInitExpressions.ToMemberAssignments(param))
+                            .GroupBy(x => x.Member)
+                            .Select(x => x.Last())),
+                parameters: param);
+        }
+
+        /// <summary>
+        /// Converts MemberInitExpressions to MemberAssignments objects
+        /// </summary>
+        private static IEnumerable<MemberAssignment> ToMemberAssignments<TSource, TDestination>(
+            this IEnumerable<Expression<Func<TSource, TDestination>>> memberInitExpressions, ParameterExpression param)
+        {
+            return from x in memberInitExpressions
+                   let replace = new ParameterReplaceVisitor(x.Parameters[0], param)
+                   from binding in ((MemberInitExpression)x.Body).Bindings.OfType<MemberAssignment>()
+                   select Expression.Bind(binding.Member, replace.VisitAndConvert(binding.Expression, "Combine"));
+        }
+
+        private class ParameterReplaceVisitor : System.Linq.Expressions.ExpressionVisitor
+        {
+            private readonly ParameterExpression from, to;
+
+            public ParameterReplaceVisitor(ParameterExpression from, ParameterExpression to)
+            {
+                this.from = from;
+                this.to = to;
+            }
+
+            protected override Expression VisitParameter(ParameterExpression node)
+            {
+                return node == from ? to : base.VisitParameter(node);
+            }
+        }
+    }
+}
---
+++ tests/Bardock.Utils.Tests/Extensions/Expressions/MemberInitExpressionExtensionsTest.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Bardock.Utils.Extensions.Expressions;
+using Xunit;
+
+namespace Bardock.Utils.Tests.Extensions
+{
+    public class MemberInitExpressionExtensionsTest
+    {
+        /// <summary>
+        /// Provides type inference when declaring a new expression
+        /// </summary>
+        private Expression<Func<T, TResult>> Expr<T, TResult>(Expression<Func<T, TResult>> exp)
+        {
+            return exp;
+        }
+
+        private class Type1 { }
+
+        private class Type2 { public int Prop1 { get; set; } }
+
+        private class Type3 : Type2 { public int Prop2 { get; set; } }
+
+        [Fact]
+        public void Do_Not_Repeat_Members()
+        {
+            const int PROP1_VALUE = 2;
+            var exp1 = Expr((Type1 x) => new Type3 { Prop1 = PROP1_VALUE });
+            var exp3 = Expr((Type1 x) => new Type3 { Prop2 = 3 });
+
+            var result = exp1.AddMembers(exp3);
+
+            AssertValid(result, prop1Value: PROP1_VALUE);
+        }
+
+        [Fact]
+        public void Repeat_Members()
+        {
+            const int PROP1_VALUE = 1;
+            var exp1 = Expr((Type1 x) => new Type3 { Prop1 = 2 });
+            var exp3 = Expr((Type1 x) => new Type3 { Prop1 = PROP1_VALUE, Prop2 = 3 });
+
+            var result = exp1.AddMembers(exp3);
+
+            AssertValid(result, prop1Value: PROP1_VALUE);
+        }
+
+        [Fact]
+        public void Use_SubClass_Do_Not_Repeat_Members()
+        {
+            const int PROP1_VALUE = 2;
+            var exp1 = Expr((Type1 x) => new Type2 { Prop1 = PROP1_VALUE });
+            var exp3 = Expr((Type1 x) => new Type3 { Prop2 = 3 });
+
+            var result = exp1.AddMembers(exp3);
+
+            AssertValid(result, prop1Value: PROP1_VALUE);
+        }
+
+        [Fact]
+        public void Use_SubClass_Repeat_Members()
+        {
+            const int PROP1_VALUE = 1;
+            var exp1 = Expr((Type1 x) => new Type2 { Prop1 = 2 });
+            var exp3 = Expr((Type1 x) => new Type3 { Prop1 = PROP1_VALUE, Prop2 = 3 });
+
+            var result = exp1.AddMembers(exp3);
+
+            AssertValid(result, prop1Value: PROP1_VALUE);
+        }
+
+        private void AssertValid(Expression<Func<Type1, Type3>> result, int prop1Value)
+        {
+            Assert.Equal(typeof(Type3), result.ReturnType);
+
+            var memberInitExpr = result.Body as MemberInitExpression;
+            Assert.NotNull(memberInitExpr);
+
+            var bindings = memberInitExpr.Bindings.Cast<MemberAssignment>();
+            Assert.Equal(2, bindings.Count());
+
+            var prop1 = bindings.FirstOrDefault(b => b.Member == typeof(Type2).GetMember("Prop1").Last());
+            Assert.NotNull(prop1);
+            Assert.Equal(prop1Value, ((ConstantExpression)prop1.Expression).Value);
+
+            var prop2 = bindings.FirstOrDefault(b => b.Member == typeof(Type3).GetMember("Prop2")[0]);
+            Assert.NotNull(prop2);
+            Assert.Equal(3, ((ConstantExpression)prop2.Expression).Value);
+        }
+    }
+}
