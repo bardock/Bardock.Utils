4bd015dbf5ab48acc5d77365b615468318e3a691
rsevil <rodrigo.sevil@gmail.com>
2015-02-11 15:10:44 -0300

    refs #7838 - Seed | Defined Project Bardock.Utils.UnitTest.Data and EF implementations

---
+++ src/Bardock.Utils.UnitTest.Data.EF/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bardock.Utils.UnitTest.Data.EF")]
+[assembly: AssemblyDescription("Entity framework implementation of IDataContextWrapper")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("bardock")]
+[assembly: AssemblyProduct("Bardock.Utils.UnitTest.Data.EF")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ca463e33-d3bf-472a-bfc0-e4ed1892a9e1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
---
+++ src/Bardock.Utils.UnitTest.Data/IDataContextWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bardock.Utils.UnitTest.Data
+{
+    public interface IDataContextWrapper
+    {
+        IQueryable<T> GetQuery<T>() 
+            where T : class;
+
+        IDataContextWrapper Add<T>(T e) 
+            where T : class;
+
+        IDataContextWrapper Update<T>(T e) 
+            where T : class;
+
+        IDataContextWrapper Save();
+
+        Task<IDataContextWrapper> SaveAsync();
+    }
+}
