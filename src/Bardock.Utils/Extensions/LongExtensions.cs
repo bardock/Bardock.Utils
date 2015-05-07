681b7acf1c27201d011aa02588f894eefb8e1888
rsevil <rodrigo.sevil@gmail.com>
2015-05-07 13:51:01 -0300

    Added LongExtensions - ToHumanFileSize method and proper Unit Tests

---
+++ src/Bardock.Utils/Extensions/LongExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Bardock.Utils.Collections;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bardock.Utils.Extensions
+{
+    public static class LongExtensions
+    {
+        public static string ToHumanFileSize(this long bytes)
+        {
+            var sizes = Coll.Array("B", "KB", "MB", "GB");
+
+            var len = bytes;
+            var order = 0;
+
+            while (len >= 1024 && order + 1 < sizes.Length)
+            {
+                order = order + 1;
+                len = len / 1024;
+            }
+
+            return string.Format("{0:0.##} {1}", len, sizes[order]);
+        }
+    }
+}
