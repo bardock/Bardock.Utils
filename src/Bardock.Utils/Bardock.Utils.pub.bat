bc723639c910371c55ed7b69c7cec2fc58e452bf
bardock <fedesanchez89@gmail.com>
2015-06-17 14:23:39 -0300

    update Bardock.Utils version

--- src/Bardock.Utils/Bardock.Utils.pub.bat
+++ src/Bardock.Utils/Bardock.Utils.pub.bat
@@ -1,6 +1,15 @@
-:: nuget spec
 set "project=Bardock.Utils"
-set /p version=Version number:
+set "localSourcePath=\\Origin-10\NuGetPackages"
+set /p version=Version number: 
+
+:: create package
 nuget pack %project%.csproj -Prop Configuration=Debug -Symbols -IncludeReferencedProjects -Version %version%
-nuget push %project%.%version%.nupkg
+
+:: publish
+:: nuget push %project%.%version%.nupkg
+
+:: if you want to publish the package to a local source, comment previous line and uncomment the following lines 
+move %project%.%version%.nupkg %localSourcePath%
+move %project%.%version%.symbols.nupkg %localSourcePath%
+
 pause;
