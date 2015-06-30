595fc44e13a5fe4c21c10119555098e704d51c69
ggalperin <ggalperin@originsoftware.com.ar>
2015-06-30 10:10:47 -0300

    add Bardock.Utils.Web.Mvc.DotNetZip 1.0.0 .net4 utils web asp.net mvc zip dotnetzip

---
+++ src/Bardock.Utils.Web.Mvc.DotNetZip/ActionResults/ZipResult.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace Bardock.Utils.Web.Mvc.DotNetZip.ActionResults
+{
+    public class ZipResult : ActionResult
+    {
+        public string FileName { get; set; }
+
+        public Entry[] Entries { get; set; }
+
+        public ZipResult(string fileName, params Entry[] entries)
+        {
+            this.Entries = entries;
+            this.FileName = fileName;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+            response.ContentType = "application/zip";
+            response.AppendHeader("content-disposition", "attachment; filename=" + FileName);
+
+            using (var zipFile = new Ionic.Zip.ZipFile())
+            {
+                foreach (var e in this.Entries)
+                {
+                    zipFile.AddEntry(e.FileName, e.BytesStream);
+                }
+                zipFile.Save(response.OutputStream);
+            }
+        }
+
+        public class Entry
+        {
+            public string FileName { get; set; }
+
+            public Stream BytesStream { get; set; }
+
+            public Entry(string fileName, Stream bytesStream)
+            {
+                this.FileName = fileName;
+                this.BytesStream = bytesStream;
+            }
+
+            public Entry(string fileName, byte[] bytes)
+                : this(fileName, new MemoryStream(bytes))
+            { }
+        }
+    }
+}
---
+++ src/Bardock.Utils.Web.Mvc.DotNetZip/Bardock.Utils.Web.Mvc.DotNetZip.pub.bat
@@ -0,0 +1,15 @@
+set "project=Bardock.Utils.Web.Mvc.DotNetZip"
+set "localSourcePath=\\Origin-10\NuGetPackages"
+set /p version=Version number: 
+
+:: create package
+nuget pack %project%.csproj -Prop Configuration=Debug -Symbols -IncludeReferencedProjects -Version %version%
+
+:: publish
+nuget push %project%.%version%.nupkg
+
+:: if you want to publish the package to a local source, comment previous line and uncomment the following lines 
+:: move %project%.%version%.nupkg %localSourcePath%
+:: move %project%.%version%.symbols.nupkg %localSourcePath%
+
+pause;
