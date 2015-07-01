e2fb4281e0a07d4af1ad856f929c2b70e7a8a064
bardock <fedesanchez89@gmail.com>
2015-07-01 09:55:26 -0300

    move LockeableObjectFactory unit tests

--- tests/Bardock.Utils.Tests/Sync/LockeableObjectFactoryTest.cs
+++ tests/Bardock.Utils.Tests/Sync/LockeableObjectFactoryTest.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Bardock.Utils.Sync;
 using Xunit;
 
-namespace Bardock.Utils.Tests.Types
+namespace Bardock.Utils.Tests.Sync
 {
     public class LockeableObjectFactoryTest
     {
