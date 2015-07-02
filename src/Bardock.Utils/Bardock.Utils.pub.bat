b320efefc3d8e38845e16c6ebd05c4f1a1e33729
bardock <fedesanchez89@gmail.com>
2015-07-02 10:35:34 -0300

    Bardock.Utils 2.6.0

--- src/Bardock.Utils/Bardock.Utils.pub.bat
+++ src/Bardock.Utils/Bardock.Utils.pub.bat
@@ -6,10 +6,10 @@ set /p version=Version number:
 nuget pack %project%.csproj -Prop Configuration=Debug -Symbols -IncludeReferencedProjects -Version %version%
 
 :: publish
-:: nuget push %project%.%version%.nupkg
+nuget push %project%.%version%.nupkg
 
 :: if you want to publish the package to a local source, comment previous line and uncomment the following lines 
-move %project%.%version%.nupkg %localSourcePath%
-move %project%.%version%.symbols.nupkg %localSourcePath%
+:: move %project%.%version%.nupkg %localSourcePath%
+:: move %project%.%version%.symbols.nupkg %localSourcePath%
 
 pause;
