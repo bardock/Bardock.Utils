f8d339db3984b1e48e11b46e11a582aaa0b19dc1
bardock <fedesanchez89@gmail.com>
2015-07-12 20:18:22 -0300

    add EmailAttribute tests

---
+++ tests/Bardock.Utils.Tests/DataAnnotations/EmailAttributeTest.cs
@@ -0,0 +1,31 @@
+﻿using Bardock.Utils.DataAnnotations;
+using Bardock.Utils.Validation;
+using FluentAssertions;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace Bardock.Utils.Tests.DataAnnotations
+{
+    public class EmailAttributeTest
+    {
+        [Fact]
+        public void ShouldBeRegularExpressionAttribute()
+        {
+            //Exercise
+            var sut = new EmailAttribute();
+
+            //Verify
+            sut.Should().BeAssignableTo<RegularExpressionAttribute>();
+        }
+
+        [Fact]
+        public void ShouldUseEmailValidatorRegex()
+        {
+            //Exercise
+            var sut = new EmailAttribute();
+
+            //Verify
+            sut.Pattern.Should().Be(EmailValidator.REGEX_PATTERN);
+        }
+    }
+}
