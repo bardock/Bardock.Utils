f987b5a731e2ef87814a6f9fd9844b9e3cceb605
bardock <fedesanchez89@gmail.com>
2015-07-12 22:25:03 -0300

    add ConvertExpressionExtensionsTest.cs with code snippet

---
+++ tests/Bardock.Utils.Tests/Extensions/Expressions/ConvertExpressionExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using Bardock.Utils.Extensions;
+using Bardock.Utils.Types;
+using FluentAssertions;
+using Xunit;
+
+namespace Bardock.Utils.Tests.Extensions
+{
+    public class ConvertExpressionExtensionsTest
+    {
+        //TODO
+        //[Fact]
+        public void RemoveConvert_CastingExpression_ShouldReturnTrue()
+        {
+            //Setup
+            var expr = Lambda.Expr<object>(() => true);
+
+            //Exercise
+            var actual = expr.RemoveConvert();
+            
+            //Verify
+            actual.Should().NotBeNull();
+        }
+    }
+}
