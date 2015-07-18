8a64188491a8ff18b0189384876b24a4dd582e4c
bardock <fedesanchez89@gmail.com>
2015-07-18 16:00:38 -0300

    add ConvertExpressionExtensions tests using auto cast expressions

--- tests/Bardock.Utils.Tests/Extensions/Expressions/ConvertExpressionExtensionsTest.cs
+++ tests/Bardock.Utils.Tests/Extensions/Expressions/ConvertExpressionExtensionsTest.cs
@@ -1,24 +1,39 @@
 ﻿using Bardock.Utils.Extensions;
 using Bardock.Utils.Types;
 using FluentAssertions;
+using System.Linq.Expressions;
 using Xunit;
 
 namespace Bardock.Utils.Tests.Extensions
 {
     public class ConvertExpressionExtensionsTest
     {
-        //TODO
-        //[Fact]
-        public void RemoveConvert_CastingExpression_ShouldReturnTrue()
+        [Fact]
+        public void RemoveConvert_FromConstantLambdaExpressionWithAutoCast_ShouldBeConstantExpression()
         {
             //Setup
             var expr = Lambda.Expr<object>(() => true);
 
             //Exercise
-            var actual = expr.RemoveConvert();
+            var actual = expr.Body.RemoveConvert();
             
             //Verify
-            actual.Should().NotBeNull();
+            actual.Should().NotBeNull().And.BeOfType<ConstantExpression>()
+                .Which.Value.Should().Be(true);
+        }
+
+        [Fact]
+        public void RemoveConvert_FromLambdaExpressionWithAndAlsoOperationAndAutoCast_ShouldBeAndAlsoBinaryExpression()
+        {
+            //Setup
+            var expr = Lambda.Expr<string, object>(x => x.IsNormalized() && x.Length == 1);
+
+            //Exercise
+            var actual = expr.Body.RemoveConvert();
+
+            //Verify
+            actual.Should().NotBeNull().And.BeAssignableTo<BinaryExpression>()
+                .Which.NodeType.Should().Be(ExpressionType.AndAlso);
         }
     }
 }
