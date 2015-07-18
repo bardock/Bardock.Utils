6e259b235a4251ab6d0ba036ca23b9adc104f554
bardock <fedesanchez89@gmail.com>
2015-07-18 17:20:27 -0300

    support FieldExpression in MemberExpressionExtensions.ToExpression() and add unit tests

--- src/Bardock.Utils/Extensions/Expressions/MemberExpressionExtensions.cs
+++ src/Bardock.Utils/Extensions/Expressions/MemberExpressionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq.Expressions;
+﻿using System;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace Bardock.Utils.Extensions
@@ -7,14 +8,27 @@ namespace Bardock.Utils.Extensions
     {
         public static MemberExpression ToExpression(this PropertyInfo prop, string parameterName = "x")
         {
-            ParameterExpression parameter = Expression.Parameter(prop.DeclaringType, "x");
+            if (prop == null)
+                throw new ArgumentException("prop is null");
+
+            var parameter = Expression.Parameter(prop.DeclaringType, "x");
             return Expression.Property(parameter, prop);
         }
 
+        [Obsolete("Please use propInfo.ToExpression().ToLambda()")]
         public static LambdaExpression ToLambdaExpression(this PropertyInfo prop, string parameterName = "x")
         {
             var expr = prop.ToExpression(parameterName: parameterName);
             return Expression.Lambda(expr, (ParameterExpression)expr.Expression);
+        }
+
+        public static MemberExpression ToExpression(this FieldInfo field, string parameterName = "x")
+        {
+            if (field == null)
+                throw new ArgumentException("field is null");
+
+            var parameter = Expression.Parameter(field.DeclaringType, "x");
+            return Expression.Field(parameter, field);
         }
     }
 }
---
+++ tests/Bardock.Utils.Tests/Extensions/Expressions/MemberExpressionExtensionsTest.cs
@@ -0,0 +1,74 @@
+﻿using Bardock.Utils.Extensions;
+using Bardock.Utils.Types;
+using FluentAssertions;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Bardock.Utils.Tests.Extensions
+{
+    public class MemberExpressionExtensionsTest
+    {
+        public class Dummy
+        {
+            public string Prop { get; set; }
+            public string Field;
+        }
+
+        [Fact]
+        public void ToExpression_FromPropertyInfo_ShouldBeMemberExpressionOfSameProp()
+        {
+            //Setup
+            var prop = typeof(Dummy).GetProperty("Prop");
+
+            //Exercise
+            var actual = prop.ToExpression();
+
+            //Verify
+            actual.Should().NotBeNull().And.BeAssignableTo<MemberExpression>()
+                .Which.Member.Should().BeSameAs(prop);
+        }
+
+        [Fact]
+        public void ToExpression_FromNullPropertyInfo_ShouldThrowArgumentException()
+        {
+            //Setup
+            PropertyInfo prop = null;
+
+            //Exercise
+            Action act = () => prop.ToExpression();
+
+            //Verify
+            act.ShouldThrow<ArgumentException>().WithMessage("prop is null");
+        }
+
+        [Fact]
+        public void ToExpression_FromFieldInfo_ShouldBeMemberExpressionOfSameField()
+        {
+            //Setup
+            var field = typeof(Dummy).GetField("Field");
+
+            //Exercise
+            var actual = field.ToExpression();
+
+            //Verify
+            actual.Should().NotBeNull().And.BeAssignableTo<MemberExpression>()
+                .Which.Member.Should().BeSameAs(field);
+        }
+
+        [Fact]
+        public void ToExpression_FromNullFieldInfo_ShouldThrowArgumentException()
+        {
+            //Setup
+            FieldInfo field = null;
+
+            //Exercise
+            Action act = () => field.ToExpression();
+
+            //Verify
+            act.ShouldThrow<ArgumentException>().WithMessage("field is null");
+        }
+    }
+}
