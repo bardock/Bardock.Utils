7cf071584f7544d8a9ab93192a9717f77597a56b
jcorrea-origin <jcorrea@originsoftware.com.ar>
2015-08-14 10:06:50 -0300

    * add IListExtensions class that allow add elements to the List at the specified index

---
+++ src/Bardock.Utils/Extensions/IListExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Bardock.Utils.Extensions
+{
+    public static class IListExtensions
+    {
+
+        /// <summary>
+        /// Adds an item to the List
+        /// </summary>
+        public static IList<TSource> AddItem<TSource>(this IList<TSource> source, TSource item)
+        {
+            source.Add(item);
+            return source;
+        }
+
+        /// <summary>
+        /// Inserts an item to the List at the specified
+        //  index
+        /// </summary>
+        public static IList<TSource> InsertItem<TSource>(this IList<TSource> source, TSource item, int index)
+        {
+            source.Insert(index, item);
+            return source;
+        }
+    }
+}
