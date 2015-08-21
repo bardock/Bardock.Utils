151da09561ff4b9cc1dbc4bcdb1846ec4876c998
rsevil <rodrigo.sevil@gmail.com>
2015-08-21 18:22:29 -0300

    UrlHelperExtensions

--- src/Bardock.Utils.Web.Mvc/Bardock.Utils.Web.Mvc.pub.bat
+++ src/Bardock.Utils.Web.Mvc/Bardock.Utils.Web.Mvc.pub.bat
@@ -1,5 +1,15 @@
-:: nuget spec
-set /p version=Version number:
-nuget pack Bardock.Utils.Web.Mvc.csproj -Prop Configuration=Debug -Symbols -IncludeReferencedProjects -Version %version%
-nuget push Bardock.Utils.Web.Mvc.%version%.nupkg
+set "project=Bardock.Utils.Web.Mvc"
+set "localSourcePath=\\Origin-10\NuGetPackages"
+set /p version=Version number: 
+
+:: create package
+nuget pack %project%.csproj -Prop Configuration=Debug -Symbols -IncludeReferencedProjects -Version %version%
+
+:: publish
+:: nuget push %project%.%version%.nupkg
+
+:: if you want to publish the package to a local source, comment previous line and uncomment the following lines 
+move %project%.%version%.nupkg %localSourcePath%
+move %project%.%version%.symbols.nupkg %localSourcePath%
+
 pause;
