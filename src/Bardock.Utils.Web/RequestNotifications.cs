2bc0ff6acbfd2c6a1a6378557fd200577016c64a
rsevil <rodrigo.sevil@gmail.com>
2015-08-24 09:11:55 -0300

    JsonExtensions - Added optional parameter camelCase that defaults to false WebViewPageExtensions - Added IsAction extension method RequestNotifications - Message is now an object, added public property ContractResolver in order to select it before serialization, it defaults to DefaultContractResolver. Added check if Response headers are written HttpResponseExtensions - Created class, added AreHeadersWritten extension method

--- src/Bardock.Utils.Web/RequestNotifications.cs
+++ src/Bardock.Utils.Web/RequestNotifications.cs
@@ -1,115 +1,133 @@
-﻿using Newtonsoft.Json;
+﻿using Bardock.Utils.Web.Extensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Web;
-using Bardock.Utils.Web.Extensions;
 
 namespace Bardock.Utils.Web
 {
-	public class RequestNotifications
-	{
+    public class RequestNotifications
+    {
+        public static DefaultContractResolver ContractResolver { get; set; }
+
         private static readonly string KEY = typeof(RequestNotifications).FullName;
 
         private static HttpRequest Request { get { return HttpContext.Current.Request; } }
+
         private static HttpResponse Response { get { return HttpContext.Current.Response; } }
 
-		/// <summary>
-		/// Singleton per-request
-		/// </summary>
-		public static RequestNotifications Instance 
+        /// <summary>
+        /// Singleton per-request
+        /// </summary>
+        public static RequestNotifications Instance
         {
-			get 
+            get
             {
-				if (HttpContext.Current.Items[KEY] == null)
-					HttpContext.Current.Items[KEY] = new RequestNotifications();
-				return (RequestNotifications)HttpContext.Current.Items[KEY];
-			}
-		}
+                if (HttpContext.Current.Items[KEY] == null)
+                    HttpContext.Current.Items[KEY] = new RequestNotifications();
+                return (RequestNotifications)HttpContext.Current.Items[KEY];
+            }
+        }
 
-		public List<Item> Items { get; set; }
+        public List<Item> Items { get; set; }
 
-		private RequestNotifications()
-		{
-			InitItems();
-		}
+        private RequestNotifications()
+        {
+            ContractResolver = new DefaultContractResolver();
+            InitItems();
+        }
 
-		/// <summary>
-		/// Tries deserialize persisted state
-		/// </summary>
-		private void InitItems()
-		{
-			string json = Request.Cookies.GetValue(KEY);
-			if (!string.IsNullOrWhiteSpace(json)) 
+        private JsonSerializerSettings BuildJsonSettings()
+        {
+            return new JsonSerializerSettings() { ContractResolver = ContractResolver };
+        }
+
+        /// <summary>
+        /// Tries deserialize persisted state
+        /// </summary>
+        private void InitItems()
+        {
+            string json = Request.Cookies.GetValue(KEY);
+            if (!string.IsNullOrWhiteSpace(json))
             {
-				try 
+                try
                 {
-					this.Items = JsonConvert.DeserializeObject<List<Item>>(json);
-				} 
-                catch (Exception ex) 
+                    this.Items = JsonConvert.DeserializeObject<List<Item>>(json, BuildJsonSettings());
+                }
+                catch (Exception ex)
                 {
-					Logger.LogManager.Default.GetLog(this).Error(ex);
-				} 
-                finally 
+                    Logger.LogManager.Default.GetLog(this).Error(ex);
+                }
+                finally
                 {
-					Response.Cookies.Expire(KEY);
-				}
-				return;
-			}
-			this.Items = new List<Item>();
-		}
+                    if (!Response.AreHeadersWritten())
+                    {
+                        Response.Cookies.Expire(KEY);
+                    }
+                }
+                return;
+            }
+            this.Items = new List<Item>();
+        }
 
-		/// <summary>
-		/// Stores current notifications in cookies, so following request can access them
-		/// </summary>
-		public void PersistState()
-		{
-            if (this.Items.Count > 0)
+        /// <summary>
+        /// Stores current notifications in cookies, so following request can access them
+        /// </summary>
+        public void PersistState()
+        {
+            if (this.Items.Count > 0 && !Response.AreHeadersWritten())
             {
                 //TODO: Remove Newtonsoft dependency
-                string json = JsonConvert.SerializeObject(this.Items);
+                string json = JsonConvert.SerializeObject(this.Items, BuildJsonSettings());
                 Response.Cookies.Add(KEY, json, DateTime.Now.AddMinutes(1));
             }
-		}
+        }
 
-		public void AddInfo(string message)
-		{
-			Items.Add(new Item(message, NotificationType.INFO));
-		}
+        public void Add(object data, NotificationType type)
+        {
+            Items.Add(new Item(data, type));
+        }
 
-		public void AddSuccess(string message)
-		{
-			Items.Add(new Item(message, NotificationType.SUCCESS));
-		}
+        public void AddInfo(object message)
+        {
+            Add(message, NotificationType.INFO);
+        }
 
-		public void AddError(string message)
-		{
-			Items.Add(new Item(message, NotificationType.ERR));
-		}
+        public void AddSuccess(object message)
+        {
+            Add(message, NotificationType.SUCCESS);
+        }
 
-		public void AddWarning(string message)
-		{
-			Items.Add(new Item(message, NotificationType.WARNING));
-		}
+        public void AddError(object message)
+        {
+            Add(message, NotificationType.ERR);
+        }
 
-		public class Item
-		{
-			public string Message { get; set; }
-			public NotificationType Type { get; set; }
+        public void AddWarning(object message)
+        {
+            Add(message, NotificationType.WARNING);
+        }
 
-			public Item(string message, NotificationType type)
-			{
-				this.Message = message;
-				this.Type = type;
-			}
-		}
+        public class Item
+        {
+            public object Message { get; set; }
 
-		public enum NotificationType
-		{
-			INFO,
-			SUCCESS,
-			ERR,
-			WARNING
-		}
-	}
+            public NotificationType Type { get; set; }
+
+            public Item(object message, NotificationType type)
+            {
+                this.Message = message;
+                this.Type = type;
+            }
+        }
+
+        public enum NotificationType
+        {
+            INFO,
+            SUCCESS,
+            ERR,
+            WARNING
+        }
+    }
 }
