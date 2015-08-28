80440a1d954a53493c6e4a67d91817e663752f56
rsevil <rodrigo.sevil@gmail.com>
2015-08-28 16:03:48 -0300

    Removed projects that were migrated to Bardock.UnitTesting

--- src/Bardock.Utils.UnitTest.Data.EF.Effort.DataLoaders/Bardock.Utils.UnitTest.Data.EF.Effort.DataLoaders.pub.bat
+++
@@ -1,5 +0,0 @@
-:: nuget spec
-set /p version=Version number:
-nuget pack Bardock.Utils.UnitTest.Data.EF.Effort.DataLoaders.csproj -Prop Configuration=Debug -Symbols -IncludeReferencedProjects -Version %version%
-nuget push Bardock.Utils.UnitTest.Data.EF.Effort.DataLoaders.%version%.nupkg
-pause;
--- src/Bardock.Utils.UnitTest.Data.EF/Bardock.Utils.UnitTest.Data.EF.pub.bat
+++
@@ -1,5 +0,0 @@
-:: nuget spec
-set /p version=Version number:
-nuget pack Bardock.Utils.UnitTest.Data.EF.csproj -Prop Configuration=Debug -Symbols -IncludeReferencedProjects -Version %version%
-nuget push Bardock.Utils.UnitTest.Data.EF.%version%.nupkg
-pause;
