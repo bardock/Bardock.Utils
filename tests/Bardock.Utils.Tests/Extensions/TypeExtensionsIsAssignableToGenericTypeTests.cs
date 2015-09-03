80c0ab75e593c6a9295ea08efe4bffa34b3acf55
bardock <fedesanchez89@gmail.com>
2015-09-03 16:06:10 -0300

    add TypeExtensions.IsAssignableToGenericType: Determines whether the given type is assignable to a generic type taking into account generic definitions (e.g., IFoo<int> is assignable to IFoo<>)

---
+++ tests/Bardock.Utils.Tests/Extensions/TypeExtensionsIsAssignableToGenericTypeTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Bardock.Utils.Extensions;
+using FluentAssertions;
+using Xunit;
+
+namespace Bardock.Utils.Tests.Extensions
+{
+    public class TypeExtensionsIsAssignableToGenericTypeTests
+    {
+        private interface IFoo<T> { }
+
+        private class Foo<T> : IFoo<T> { }
+
+        private class Bar : IFoo<int> { }
+
+        [Theory]
+        [InlineData(typeof(Foo<>), typeof(IFoo<>))]
+        [InlineData(typeof(IFoo<>), typeof(IFoo<>))]
+        [InlineData(typeof(Foo<>), typeof(Foo<>))]
+        [InlineData(typeof(IFoo<int>), typeof(IFoo<>))]
+        [InlineData(typeof(Foo<int>), typeof(IFoo<>))]
+        [InlineData(typeof(Foo<int>), typeof(Foo<>))]
+        [InlineData(typeof(Bar), typeof(IFoo<>))]
+        public void ValidParams_ShouldReturnTrue(Type givenType, Type genericType)
+        {
+            //Exercise
+            var actual = givenType.IsAssignableToGenericType(genericType);
+
+            //Verify
+            actual.Should().Be(true);
+        }
+
+        [Theory]
+        [InlineData(typeof(IFoo<>), typeof(Foo<>))]
+        [InlineData(typeof(IFoo<int>), typeof(Foo<>))]
+        [InlineData(typeof(IFoo<int>), typeof(IFoo<object>))]
+        [InlineData(typeof(IFoo<object>), typeof(IFoo<int>))]
+        public void InvalidParams_ShouldReturnTrue(Type givenType, Type genericType)
+        {
+            //Exercise
+            var actual = givenType.IsAssignableToGenericType(genericType);
+
+            //Verify
+            actual.Should().Be(false);
+        }
+    }
+}
